64799d05a1153fca7a68197f021e762f70163e44
Mateusz <135542253+Mateusz42@users.noreply.github.com>
2023-12-14 00:49:12 +0100

    Add files via upload

---
+++ task-01.js
@@ -0,0 +1,6 @@
+const ListWithId = document.querySelectorAll('#categories');
+
+const numberOfCategories = ListWithId.querySelectorAll(".item");
+console.log("Number of categories: " + menuItemsByTagName.length);
+
+console.log(menuItemsByTagName);
